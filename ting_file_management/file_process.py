d87f2fa9f67c2c9fa59bf66a83fa1753e2b85cfa
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-03-11 19:07:31 -0300

    completed requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in range(len(instance)):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return
+
+    imported_file = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(imported_file),
+        "linhas_do_arquivo": imported_file,
+    }
+
+    instance.enqueue(result)
+    print(result)
 
 
 def remove(instance):
