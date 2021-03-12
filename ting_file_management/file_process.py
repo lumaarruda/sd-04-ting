9f0a08f9c22cb4410ee684115c8bb9ad94a0eb2f
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-03-12 20:39:35 -0300

    flake8 fix

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,9 +3,12 @@ import sys
 
 
 def stdout_file(file_info):
-    print(f"""'nome_do_arquivo': '{file_info["nome_do_arquivo"]}',
+    print(
+        f"""'nome_do_arquivo': '{file_info["nome_do_arquivo"]}',
         'qtd_linhas': {len(file_info["linhas_do_arquivo"])},
-        'linhas_do_arquivo': {file_info["linhas_do_arquivo"]}""", file=sys.stdout)
+        'linhas_do_arquivo': {file_info["linhas_do_arquivo"]}""",
+        file=sys.stdout,
+    )
 
 
 def process(path_file, instance):
@@ -13,7 +16,7 @@ def process(path_file, instance):
     file_info = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(text),
-        "linhas_do_arquivo": text
+        "linhas_do_arquivo": text,
     }
     if instance.enqueue(file_info):
         stdout_file(file_info)
@@ -22,7 +25,10 @@ def process(path_file, instance):
 def remove(instance):
     file_removed = instance.dequeue()
     if file_removed:
-        print(f"Arquivo {file_removed['nome_do_arquivo']} removido com sucesso", file=sys.stdout)
+        print(
+            f"Arquivo {file_removed['nome_do_arquivo']} removido com sucesso",
+            file=sys.stdout,
+        )
     else:
         print("Não há elementos", file=sys.stdout)
 
