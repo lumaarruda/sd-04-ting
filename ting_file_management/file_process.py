c12a91406d2629983a6456493cffd619d9178e37
Josias Viveiro <josiasviveiro@gmail.com>
2021-03-12 17:39:46 -0300

    Req. 3 - file_process.py process()

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,27 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    count_same_name = 0
+    for index in range(len(instance)):
+        element = instance.search(index)
+        if path_file == element["nome_do_arquivo"]:
+            count_same_name += 1
+            break
+
+    if count_same_name == 0:
+        txt_file = txt_importer(path_file)
+
+        processed_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt_file),
+            "linhas_do_arquivo": txt_file
+        }
+
+        instance.enqueue(processed_data)
+
+        sys.stdout.write(f"{processed_data}")
 
 
 def remove(instance):
