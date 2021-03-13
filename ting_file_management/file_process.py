5566fba86467d2d612a4f804602a0391a1ff80a2
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-03-13 17:41:37 -0300

    +++++_________

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,7 +3,16 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     bof = txt_importer(path_file)
-    print(bof)
+    counterLines = 0
+    for line in bof:
+        counterLines += 1
+    arquivo = {
+         "nome_do_arquivo": path_file,
+         "qtd_linhas": counterLines,
+         "linhas_do_arquivo": bof,
+    }
+    print("arquivo", arquivo)
+    instance.enqueue(arquivo)
 
 
 def remove(instance):
