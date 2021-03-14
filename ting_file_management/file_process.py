0abc209c742300ba5c10c174914fcbd156662b56
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-03-14 15:51:55 -0300

    queue & file_management

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    importer = txt_importer(path_file)
+    process_archive = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(importer),
+        "linhas_do_arquivo": importer
+    }
+
+    instance.enqueue(process_archive)
+    print(process_archive)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
