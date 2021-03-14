ed360c591252fd38153e5a507e5b5f074d7488ab
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-03-13 21:21:04 -0300

    ___________

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,10 +2,24 @@
 
 
 def exists_word(word, instance):
-    for item in (instance):
-        print(item)
-    return []
+    toReturn = []
+    for item in range(instance.__len__()):
+        arquivo = instance.search(item)
+        counter_line = 0
+        occurencia = []
+        for item in arquivo["linhas_do_arquivo"]:
+            counter_line += 1
+            contains_word = word in item
+            if contains_word is True:
+                occurencia.append({"linha": counter_line})
+                toReturn.append({
+                        "palavra": f"{word}",
+                        "arquivo": arquivo["nome_do_arquivo"],
+                        "ocorrencias": occurencia,
+                    })
+    return toReturn
 
 
 def search_by_word(word, instance):
+    print("2nd")
     return []
