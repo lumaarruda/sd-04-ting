753f15fde9611048b3b5c9e5ee6b1430ef4ffbd3
Josias Viveiro <josiasviveiro@gmail.com>
2021-03-15 20:22:17 -0300

    Req.7 - word_search.py - search-by_word()

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -11,13 +11,46 @@ def exists_word(word, instance):
     #         }
     #     ]
     # }]
-    data = [{
-        "palavra": word,
-        "arquivo": "",
-        "ocorrencias": []
-    }]
+    data = [{"palavra": word, "arquivo": "", "ocorrencias": []}]
 
     # data_exists_word = list()
+    for index in range(len(instance)):
+        folder = instance.search(index)
+        # print("\nFOLDER:", folder)
+
+        # Arquivo
+        data[0]["arquivo"] = folder["nome_do_arquivo"]
+
+        # Ocorrências
+        for index, line in enumerate(folder["linhas_do_arquivo"]):
+            # print("\nINDEX:", index)
+            # print("LINE:", line)
+            if word in line:
+                data[0]["ocorrencias"].append({"linha": index + 1})
+
+    # print("\n DATA:", data)
+    if len(data[0]["ocorrencias"]) < 1:
+        return []
+    else:
+        return data
+
+
+def search_by_word(word, instance):
+    # data = [
+    #     {
+    #         "palavra": "",
+    #         "arquivo": "",
+    #         "ocorrencias": [
+    #             {"linha": 1, "conteudo": "Acima de tudo,"},
+    #             {
+    #                 "linha": 2,
+    #                 "conteudo": "",
+    #             },
+    #         ],
+    #     }
+    # ]
+    data = [{"palavra": word, "arquivo": "", "ocorrencias": []}]
+
     for index in range(len(instance)):
         folder = instance.search(index)
         print("\nFOLDER:", folder)
@@ -29,12 +62,11 @@ def exists_word(word, instance):
         for index, line in enumerate(folder["linhas_do_arquivo"]):
             # print("\nINDEX:", index)
             # print("LINE:", line)
-            if word in line:
-                data[0]["ocorrencias"].append(
-                    {
-                        "linha": index + 1
-                    }
-                )
+            if word.lower() in line.lower():
+                data[0]["ocorrencias"].append({
+                    "linha": index + 1,
+                    "conteudo": line
+                })
 
     # print("\n DATA:", data)
     if len(data[0]["ocorrencias"]) < 1:
@@ -43,16 +75,16 @@ def exists_word(word, instance):
         return data
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
-
-
 # --- Teste ---
 # from ting_file_management.file_process import process
 # from ting_file_management.queue import Queue
 
 # print("\n--- Process ---")
 # project = Queue()
-# process("statics/nome_pedro.txt", project)
-# print("\n\n--- Exists_Word ---")
-# word = exists_word("Pedro", project)
+# # process("statics/nome_pedro.txt", project)
+
+# # print("\n\n--- Exists_Word ---")
+# # word = exists_word("Pedro", project)
+
+# print("\n\n--- Search_By_Word ---")
+# word = search_by_word("pedro", project)
