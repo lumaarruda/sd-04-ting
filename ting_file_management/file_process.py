9fe5cbc19dc52154f515d9bba247faf2d728c2d1
juniomelos <juniomelos@gmail.com>
2021-03-16 18:14:19 -0300

    test_file_process and test_file_mangement.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,29 +3,29 @@ import sys
 
 
 def process(path_file, instance):
-    imported_text = txt_importer(path_file)
-    line = 0
-    for line in imported_text:
-        line += 1
+    imported_txt = txt_importer(path_file)
+    lines = 0
+    for line in imported_txt:
+        lines += 1
     data = {
          "nome_do_arquivo": path_file,
-         "qtd_linhas": line,
-         "linhas_do_arquivo": imported_text,
+         "qtd_linhas": lines,
+         "linhas_do_arquivo": imported_txt,
     }
-
     instance.enqueue(data)
+    sys.stdout.write(f"{data}")
 
 
 def remove(instance):
     if len(instance) == 0:
         sys.stdout.write("Não há elementos\n")
     else:
-        path_file = instance.dequeue()['nome_do_arquivo']
-        sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
+       path_file = instance.dequeue()['nome_do_arquivo']
+       sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
     try:
         instance.search(position)
     except IndexError:
-        sys.stderr.write("Posição inválida")
+        print("Posição inválida", file=sys.stderr)
