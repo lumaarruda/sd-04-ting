fd0e867ce7850801077e9b4ac95f0ede0a075bdf
juniomelos <juniomelos@gmail.com>
2021-03-16 18:35:18 -0300

    test_word_search - acho que ultimo committtt \o/

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,8 +1,9 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
-
+    # Executar a função exists_word com palavra inexistente
+    # deverá retornar uma lista vazia.
+    return []
 
 def search_by_word(word, instance):
-    # Executar a função exists_word com palavra inexistente
+    # Executar a função search_by_word com palavra inexistente
     # deverá retornar uma lista vazia.
     return []
