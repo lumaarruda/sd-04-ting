a5fa7ca335a58be0dcae98fbff5f10c2d73045bf
frederico-campello <fredfmc33@gmail.com>
2021-03-16 15:25:09 -0300

    Requisito 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = list()
+    obj = dict()
+    for item in instance.elements:
+        obj["palavra"] = word
+        obj["arquivo"] = item["nome_do_arquivo"]
+        obj["ocorrencias"] = list()
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word not in line:
+                return output
+            else:
+                obj["ocorrencias"].append({"linha": index + 1})
+    output.append(obj)
+    return output
 
 
 def search_by_word(word, instance):
