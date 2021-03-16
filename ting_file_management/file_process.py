31610ad5efcae09aec486f511b1cb743f51ef022
igorgbr <igorgphotoarte@gmail.com>
2021-03-16 19:21:51 -0300

    req 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -22,4 +22,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        instance.search(position)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
