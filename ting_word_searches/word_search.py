9d7fde631e323b8ffd1d7b0312ea69eb407ede75
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-03-16 22:00:22 -0300

    Agora fimmm

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue = instance._queue
+    count = 0
+    match = []
+    occurrences = []
+
+    for file in queue:
+        for i in file["linhas_do_arquivo"]:
+            if re.findall(word, i, re.IGNORECASE):
+                count += 1
+                occurrences.append({"linha": count})
+
+        if len(occurrences):
+            match.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences})
+
+    return match
 
 
 def search_by_word(word, instance):
