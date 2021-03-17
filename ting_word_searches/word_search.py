b79747bfa56b2bca277247ed34041a2f18e8f996
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-03-16 22:20:59 -0300

    ahhhhhhhhhhH

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,42 +2,26 @@ import re
 
 
 def exists_word(word, instance):
-    queue = instance._queue
+    list = []
     count = 0
-    match = []
-    occurrences = []
 
-    for file in queue:
-        for i in file["linhas_do_arquivo"]:
-            if re.findall(word, i, re.IGNORECASE):
-                count += 1
-                occurrences.append({"linha": count})
+    for i in range(len(instance)):
+        seach_intance = instance.search(i)
+        list_of_ocorrence = []
+        count += 1
+        for line in seach_intance["linhas_do_arquivo"]:
+            if re.search(word, line, re.IGNORECASE):
+                list_of_ocorrence.append({
+                    "linha": count,
+                })
+                list.append({
+                    "palavra": f"{word}",
+                    "arquivo": seach_intance["nome_do_arquivo"],
+                    "ocorrencias": list_of_ocorrence,
+                })
 
-        if len(occurrences):
-            match.append({
-                "palavra": word,
-                "arquivo": file["nome_do_arquivo"],
-                "ocorrencias": occurrences})
-
-    return match
+    return list
 
 
 def search_by_word(word, instance):
-    queue = instance._queue
-    match = []
-    occurrences = []
-    count = 0
-
-    for file in queue:
-        for i in file["linhas_do_arquivo"]:
-            if re.findall(word, i, re.IGNORECASE):
-                count += 1
-                occurrences.append({"linha": count, "conteudo": i})
-
-        if len(occurrences):
-            match.append({
-                "palavra": word,
-                "arquivo": file["nome_do_arquivo"],
-                "ocorrencias": occurrences})
-
-    return match
+    """Aqui irá sua implementação"""
