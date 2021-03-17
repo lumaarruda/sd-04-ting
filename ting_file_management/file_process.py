8eab4223ee4404b64a89feff02d31f326d717c3f
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-03-17 16:29:24 -0300

    ting file management

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,3 +1,5 @@
+import sys
+# from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 
 
@@ -16,7 +18,20 @@ def process(path_file, instance):
 def remove(instance):
     if len(instance) == 0:
         print("Não há elementos")
+    else:
+        actual = instance._data[0]["nome_do_arquivo"]
+        instance.dequeue()
+        print(f"Arquivo {actual} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position > len(instance):
+        print("Posição inválida", file=sys.stderr)
+    else:
+        data = instance.search(position)
+        found_archive = {
+            "nome_do_arquivo": data["nome_do_arquivo"],
+            "qtd_linhas": data["qtd_linhas"],
+            "linhas_do_arquivo": data["linhas_do_arquivo"]
+        }
+        print(found_archive)
