bda531a51e8fb8f3f88bd29ddbadc9ff3c13a6da
Tereza Cristina Carvalho <terezacristina@MacBook-Pro-de-Tereza.local>
2021-03-16 23:17:41 -0300

    só amanhã agora

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+    linhas_do_arquivo = txt_importer(path_file)
+    new_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas_do_arquivo),
+        "linhas_do_arquivo": linhas_do_arquivo,
+    }
+    instance.enqueue(new_file)
+    instance.last_file()
 
 
 def remove(instance):
