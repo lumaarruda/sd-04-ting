9b056a62ec00428c95b950798a80e811cad0d580
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-03-16 21:37:14 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def format(name, lines):
+    return {
+        "nome_do_arquivo": name,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_data = txt_importer(path_file)
+    for item in range(instance.__len__()):
+        if instance.search(item)['nome_do_arquivo'] == path_file:
+            return
+
+    dict_return = format(path_file, text_data)
+    instance.enqueue(dict_return)
+    print(dict_return, file=sys.stdout)
 
 
 def remove(instance):
