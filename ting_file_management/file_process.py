cca74ae9fa16f3fc29ecbe76873d27187c947926
lucasfranca90 <luc.rangel90@gmail.com>
2021-03-19 10:57:42 -0300

    passando local

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,28 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    same_files = 0
+    for index in range(len(instance)):
+        element = instance.search(index)
+        if path_file == element["nome_do_arquivo"]:
+            same_files += 1
+            break
+
+    if same_files == 0:
+        lines = txt_importer(path_file)
+
+        processed_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+
+        instance.enqueue(processed_data)
+
+        sys.stdout.write(f"{processed_data}")
 
 
 def remove(instance):
