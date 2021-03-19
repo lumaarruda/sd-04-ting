53e56a82e7ff940ab51312c7e8c3dea6f51e268a
gabrielukke <gabriellukk@outlook.com>
2021-03-18 22:33:47 -0300

    Req 3 - All tests

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,30 @@
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
+
+project = Queue()
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    repeated_files = 0
+    for index in range(len(instance)):
+        file = instance.search(index)
+        if path_file == file["nome_do_arquivo"]:
+            repeated_files += 1
+            break
+
+    if repeated_files == 0:
+        file_text = txt_importer(path_file)
+
+        file_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_text),
+            "linhas_do_arquivo": file_text
+        }
+
+        instance.enqueue(file_info)
+
+        sys.stdout.write(f"{file_info}")
 
 
 def remove(instance):
