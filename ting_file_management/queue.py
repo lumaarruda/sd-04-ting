86a4e314fcf02dec6f495fed7bc70fe79d805227
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 14:53:23 -0300

    Create class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.queue:
+            return None
+        else:
+            self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        return self.queue[index]:
