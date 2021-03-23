5f3987a1920f65cee4b9f8679829ac7d3d77205b
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 16:50:01 -0300

    Implement exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    if not len(instance.queue):
+        return []
+    result_list = list()
+    for file in instance.queue:
+        lines_that_occurs = list()
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word in line:
+                lines_that_occurs.append({ "linha": index + 1})
+        if len(lines_that_occurs):
+            result_file = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines_that_occurs
+            }
+            result_list.append(result_file)
+    
+    return result_list
 
 
 def search_by_word(word, instance):
