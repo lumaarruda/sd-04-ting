74e68570f3c3bcc006b94de14b1fe6324095c8aa
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 15:29:16 -0300

    Implement process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+git from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_file = txt_importer(path_file)
+    if '/' in path_file:
+        path_file = path_file.split('/')[-1]
+    
+    return {
+        'nome_do_arquivo': path_file,
+        "qtd_linhas": len(text_file),
+        "linhas_do_arquivo": text_file
+    }
 
 
 def remove(instance):
