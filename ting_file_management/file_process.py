6e866cbf687f705b7c4fcf88f764b79d6b0b5dbf
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 15:34:56 -0300

    Refactor

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,13 +4,13 @@ from .file_management import txt_importer
 
 def process(path_file, instance):
     text_file = txt_importer(path_file)
-    result = {
+    processed_file = {
         'nome_do_arquivo': path_file,
         "qtd_linhas": len(text_file),
         "linhas_do_arquivo": text_file
     }
 
-    print(f"{result}", file=sys.stdout)
+    return print(f"{processed_file}", file=sys.stdout)
 
 
 def remove(instance):
