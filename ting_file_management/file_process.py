eeeb274be391281d276910f6ade42e728bbbb684
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 15:53:10 -0300

    Implement remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,19 +5,21 @@ from .file_management import txt_importer
 def process(path_file, instance):
     text_file = txt_importer(path_file)
     processed_file = {
-        'nome_do_arquivo': path_file,
+        "nome_do_arquivo": path_file,
         "qtd_linhas": len(text_file),
         "linhas_do_arquivo": text_file
     }
 
     if (instance.enqueue(processed_file)):
         return print(f"{processed_file}", file=sys.stdout)
-    else:
-        return None
+    return None
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    removed_file = instance.dequeue()
+    if not removed_file:
+        return print("Não há elementos")
+    print(f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
