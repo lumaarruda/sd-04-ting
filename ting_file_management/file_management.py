a65daf1c8bcdf2df60e80e5290e21a133d83017d
Victor L <victor.lustosacp@gmail.com>
2021-03-23 13:34:53 -0300

    req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -9,7 +9,8 @@ def txt_importer(path_file):
                 for line in file:
                     data.append(line.strip())
         except:
-            return FileNotFoundError(sys.stderr.write(f"Arquivo {path_file} não encontrado\n"))
+            return FileNotFoundError(sys.stderr.write(
+                f"Arquivo {path_file} não encontrado\n"))
         return data
     else:
         return sys.stderr.write("Formato inválido\n")
