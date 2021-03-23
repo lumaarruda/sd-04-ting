cb278d1520f6c1623dd7ac9f08f98e4838764df3
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 16:57:14 -0300

    Format

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,4 @@
-def exists_word(word, instance):
+def search_lines_by_word(word, instance, show_content=False):
     if not len(instance.queue):
         return []
     result_list = list()
@@ -6,7 +6,7 @@ def exists_word(word, instance):
         lines_that_occurs = list()
         for index, line in enumerate(file["linhas_do_arquivo"]):
             if word in line:
-                lines_that_occurs.append({ "linha": index + 1})
+                lines_that_occurs.append({"linha": index + 1})
         if len(lines_that_occurs):
             result_file = {
                 "palavra": word,
@@ -16,6 +16,9 @@ def exists_word(word, instance):
             result_list.append(result_file)
 
     return result_list
+
+def exists_word(word, instance):
+    return search_lines_by_word(word, instance)
 
 
 def search_by_word(word, instance):
