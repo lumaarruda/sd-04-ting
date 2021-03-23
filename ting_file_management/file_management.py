b02eb1a9911839ce487c283be27dd662ad4cc83d
Victor L <victor.lustosacp@gmail.com>
2021-03-23 13:34:12 -0300

    req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,5 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-queue.enqueue(42)
-    queue.enqueue(43)
-    queue.enqueue(44)
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                data = list()
+                for line in file:
+                    data.append(line.strip())
+        except:
+            return FileNotFoundError(sys.stderr.write(f"Arquivo {path_file} não encontrado\n"))
+        return data
+    else:
+        return sys.stderr.write("Formato inválido\n")
