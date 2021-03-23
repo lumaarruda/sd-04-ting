fe99e2acb7d13c71b29e920500aed0a877b648c3
danielPantalena <pantalenadaniel@gmail.com>
2021-03-23 15:33:29 -0300

    Pass process test

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,16 +1,16 @@
-git from .file_management import txt_importer
+import sys
+from .file_management import txt_importer
 
 
 def process(path_file, instance):
     text_file = txt_importer(path_file)
-    if '/' in path_file:
-        path_file = path_file.split('/')[-1]
-    
-    return {
+    result = {
         'nome_do_arquivo': path_file,
         "qtd_linhas": len(text_file),
         "linhas_do_arquivo": text_file
     }
+
+    print(f"{result}", file=sys.stdout)
 
 
 def remove(instance):
