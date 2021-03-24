9267c1bcaf5a7515d9e6d7aedf17c7c0fdb06d33
TIAGO Emanuel <tiago@pop-os.localdomain>
2021-03-24 19:49:49 -0300

    req. 6 parte 1

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,17 +4,26 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-    rows = txt_importer(path_file)
 
-    file_data = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(rows),
-        "linhas_do_arquivo": rows
-    }
+    file_data = dict()
+    count_file = 0
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        if path_file == file["nome_do_arquivo"]:
+            count_file += 1
+            return
+
+    if count_file == 0:
+        file_txt = txt_importer(path_file)
+        print('TEste', file_txt)
+        file_data["nome_do_arquivo"] = path_file
+        file_data["qtd_linhas"] = len(file_txt)
+        file_data["linhas_do_arquivo"] = file_txt
 
     instance.enqueue(file_data)
-    sys.stdout.write(f"{file_data}\n")
-    return file_data
+    sys.stdout.write(f"{file_data}")
+    # return file_data
 
 
 def remove(instance):
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,28 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
 
+    obj_file = [{
+        "palavra": word,
+        "arquivo": "",
+        "ocorrencias": []
+    }]
+
+    for element in range(len(instance)):
+        file = instance.search(element)
+        # print('teste', file)
+
+        obj_file[0]["arquivo"] = file["nome_do_arquivo"]
+        # obj_file["ocorrencias"] = list()
+        # obj_file["palavra"] = word
+        for index, row in enumerate(file["linhas_do_arquivo"]):
+            if word in row:
+                obj_file[0]["ocorrencias"].append({"Linha": index + 1})
+
+        if len(obj_file[0]["ocorrencias"]) < 1:
+            return []
+        else:
+            return obj_file
+
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
