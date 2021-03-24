a2ddbad7d6e9f8c625a3322f5c0a68723b3fdb50
ribaslucas <lucas@mundoribas.com.br>
2021-03-24 12:40:50 -0300

    Nova tentativa - agora separando as funções

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,25 +1,19 @@
-def exists_word(word, instance):
-    result = []
-    for i in range(len(instance)):
-        entry = instance.search(i)
-        occurrences = []
-        for index, line in enumerate(entry['linhas_do_arquivo']):
-            if word.lower() in line.lower():
-                occurrences.append({"linha": index + 1})
-        if occurrences == []:
-            return occurrences
-        result.append({
-            "arquivo": entry['nome_do_arquivo'],
-            "ocorrencias": occurrences,
-            "palavra": word
-        })
-    return result
+def search_on_line(word, file_info, should_add_content):
+    data = []
+    for index in range(len(file_info["linhas_do_arquivo"])):
+        line = file_info["linhas_do_arquivo"][index]
+        if word.lower() in line.lower():
+            occur = {"linha": index + 1}
+            if should_add_content:
+                occur["conteudo"] = line
+            data.append(occur)
+    return data
 
 
-def search_on_instance(word, instance, add_content=False):
+def search_on_instance(word, instance, should_add_content=False):
     files_found = []
     for file_info in instance:
-        occurrences = exists_word(word, file_info, add_content)
+        occurrences = search_on_line(word, file_info, should_add_content)
 
         if occurrences:
             files_found.append(
@@ -33,6 +27,26 @@ def search_on_instance(word, instance, add_content=False):
     return files_found
 
 
+def exists_word(word, instance):
+    return search_on_instance(word, instance)
+
+
 def search_by_word(word, instance):
     return search_on_instance(word, instance, True)
 
+# def exists_word(word, instance):
+    #     result = []
+#     for i in range(len(instance)):
+#         entry = instance.search(i)
+#         occurrences = []
+#         for index, line in enumerate(entry['linhas_do_arquivo']):
+#             if word.lower() in line.lower():
+#                 occurrences.append({"linha": index + 1})
+#         if occurrences == []:
+#             return occurrences
+#         result.append({
+#             "arquivo": entry['nome_do_arquivo'],
+#             "ocorrencias": occurrences,
+#             "palavra": word
+#         })
+#     return result
