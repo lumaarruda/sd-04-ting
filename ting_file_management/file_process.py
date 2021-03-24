7810661737d3ad4b3f1c5f3e9b7a93b9ef059513
Ronald Lima <ronaldazevedolima@gmail.com>
2021-03-24 16:48:28 -0300

    sera??????

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,11 +1,10 @@
 from ting_file_management.file_management import txt_importer
+# from ting_file_management.queue import Queue
 import sys
 
 
-lidos = set()
-
-
 def process(path_file, instance):
+    lidos = set()
     result = {
         'nome_do_arquivo': '',
         'qtd_linhas': '',
@@ -21,7 +20,7 @@ def process(path_file, instance):
         instance.enqueue(result)
         return print(result, file=sys.stdout)
     else:
-        pass
+        return
 
 
 def remove(instance):
@@ -41,3 +40,11 @@ def file_metadata(instance, position):
         instance.search(position)
     except IndexError:
         print("Posição inválida", file=sys.stderr)
+
+
+# projeto = Queue()
+# caminho = "statics/arquivo_teste.txt"
+
+
+# print(f"primeiro {process(caminho, projeto)}")
+# print(f"segundo {process(caminho, projeto)}")
