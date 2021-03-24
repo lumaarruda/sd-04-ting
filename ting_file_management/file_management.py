fce15d468f60aead551af7ac22a552988a6903e0
lourenco <loulage@gmail.com>
2021-03-24 17:21:51 -0300

    implemented txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -11,4 +11,14 @@ import sys
 
 
 def txt_importer(path_file):
-    
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file) as file:
+            content = file.read()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado")
+
+    else:
+        return content.split("\n")
