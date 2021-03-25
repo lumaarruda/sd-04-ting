39551d6b2a35f1dcc8fba50ca2c2f3594918f728
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-03-25 20:20:14 -0300

    req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    for item in range(len(instance)):
+        element = instance.search(item)
+        if path_file == element["nome_do_arquivo"]:
+            return
+
+    file = txt_importer(path_file)
+
+    result_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    print(result_file)
+    instance.enqueue(result_file)
 
 def remove(instance):
     """Aqui irá sua implementação"""
