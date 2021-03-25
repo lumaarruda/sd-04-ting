714c4ce79212a312a403ef41110815e6610b3648
djonata-dias <diasdjonata@gmail.com>
2021-03-25 08:05:05 -0300

    deacreasing clomplex code

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,20 +1,26 @@
-import sys
+
+
+def search_ocurrences(word, file_search, add_content):
+    list_ocurrences = list()
+    for linha in range(len(file_search["linhas_do_arquivo"])):
+
+        if word.lower() in file_search["linhas_do_arquivo"][linha].lower():
+            if add_content:
+                list_ocurrences.append({"linha": linha + 1,
+                                        "conteudo": file_search["linhas_do_arquivo"][linha]})
+            else:
+                list_ocurrences.append({"linha": linha + 1})
+
+    return list_ocurrences
 
 
 def search_word(word, instance, add_content):
     list_files = list()
+
     for index in range(len(instance)):
         file_search = instance.search(index)
-        list_ocurrences = list()
+        list_ocurrences = search_ocurrences(word, file_search, add_content)
 
-        conteudo = file_search["linhas_do_arquivo"]
-        for linha in range(len(conteudo)):
-            if word.lower() in conteudo[linha].lower():
-                if add_content:
-                    list_ocurrences.append({"linha": linha + 1,
-                                            "conteudo": conteudo[linha]})
-                else:
-                    list_ocurrences.append({"linha": linha + 1})
         if len(list_ocurrences) > 0:
             list_files.append({
                 "palavra": word,
