bfb9916a37904a048615b260949d20427f320189
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-03-25 20:48:33 -0300

    refactor

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,35 +1,25 @@
-def search_on_line(word, file_info, add_content):
+def exists_word(word, instance):
     result = []
-    for index in range(len(file_info["linhas_do_arquivo"])):
-        file_lines = file_info["linhas_do_arquivo"][index]
-        if word.lower() in file_lines.lower():
-            word_occurrence = {"linha": index + 1}
-            if add_content:
-                word_occurrence["conteudo"] = file_lines
-            result.append(word_occurrence)
+    counter = 0
+    word_occurrence = []
+
+    for item in range(instance.__len__()):
+        file = instance.search(item)
+
+        for line in file["linhas_do_arquivo"]:
+            counter += 1
+            has_word = word in line
+            if has_word:
+                word_occurrence.append({"linha": counter})
+                result.append(
+                    {
+                        "palavra": f"{word}",
+                        "arquivo": file["nome_do_arquivo"],
+                        "ocorrencias": word_occurrence,
+                    }
+                )
     return result
 
 
-def search_on_instance(word, instance, add_content=False):
-    files_found = []
-    for file_info in instance:
-        word_occurrence = search_on_line(word, file_info, add_content)
-
-        if word_occurrence:
-            files_found.append(
-                {
-                    "palavra": word,
-                    "arquivo": file_info["nome_do_arquivo"],
-                    "ocorrencias": word_occurrence,
-                }
-            )
-
-    return files_found
-
-
-def exists_word(word, instance):
-    return search_on_instance(word, instance)
-
-
 def search_by_word(word, instance):
-    return search_on_instance(word, instance, True)
+    return []
