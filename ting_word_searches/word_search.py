3f6b54b6fb624aa852f7e8a88e7fedfbf9e8c36d
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-03-25 20:42:03 -0300

    done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,35 @@
+def search_on_line(word, file_info, add_content):
+    result = []
+    for index in range(len(file_info["linhas_do_arquivo"])):
+        file_lines = file_info["linhas_do_arquivo"][index]
+        if word.lower() in file_lines.lower():
+            word_occurrence = {"linha": index + 1}
+            if add_content:
+                word_occurrence["conteudo"] = file_lines
+            result.append(word_occurrence)
+    return result
+
+
+def search_on_instance(word, instance, add_content=False):
+    files_found = []
+    for file_info in instance:
+        word_occurrence = search_on_line(word, file_info, add_content)
+
+        if word_occurrence:
+            files_found.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_info["nome_do_arquivo"],
+                    "ocorrencias": word_occurrence,
+                }
+            )
+
+    return files_found
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_on_instance(word, instance)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_on_instance(word, instance, True)
