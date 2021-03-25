284ef99d65a36e32ecaafe3d3fa102813ba7aeb3
djonata-dias <diasdjonata@gmail.com>
2021-03-25 07:53:32 -0300

    word search implement done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,33 @@
+import sys
+
+
+def search_word(word, instance, add_content):
+    list_files = list()
+    for index in range(len(instance)):
+        file_search = instance.search(index)
+        list_ocurrences = list()
+
+        conteudo = file_search["linhas_do_arquivo"]
+        for linha in range(len(conteudo)):
+            if word.lower() in conteudo[linha].lower():
+                if add_content:
+                    list_ocurrences.append({"linha": linha + 1,
+                                            "conteudo": conteudo[linha]})
+                else:
+                    list_ocurrences.append({"linha": linha + 1})
+        if len(list_ocurrences) > 0:
+            list_files.append({
+                "palavra": word,
+                "arquivo": file_search["nome_do_arquivo"],
+                "ocorrencias": list_ocurrences
+            })
+
+    return list_files
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_word(word, instance, False)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_word(word, instance, True)
