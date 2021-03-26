d45a266fcdaf326f20a4e3c8bccba6d7e3eaebf8
lourenco <loulage@gmail.com>
2021-03-26 02:53:18 -0300

    bugfix -2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,22 +1,22 @@
-def search_words_on_line(word, file_info, add_content):
-    result = []
+def search_in_lines(word, file_info, add_content):
+    occurrences = []
     for index in range(len(file_info["linhas_do_arquivo"])):
         line = file_info["linhas_do_arquivo"][index]
         if word.lower() in line.lower():
-            occurrences = {"linha": index + 1}
+            occurr = {"linha": index + 1}
             if add_content:
-                occurrences["conteudo"] = line
-            result.append(occurrences)
-    return result
+                occurr["conteudo"] = line
+            occurrences.append(occurr)
+    return occurrences
 
 
-def search_words_on_instance(word, instance, add_content=False):
+def search_on_instance(word, instance, add_content=False):
     files_found = []
     for file_info in instance:
-        occurrences = search_words_on_line(word, file_info, add_content)
+        occurrences = search_in_lines(word, file_info, add_content)
 
-        if occurrences:
-            files_found.append(
+    if occurrences:
+        files_found.append(
                 {
                     "palavra": word,
                     "arquivo": file_info["nome_do_arquivo"],
@@ -28,8 +28,8 @@ def search_words_on_instance(word, instance, add_content=False):
 
 
 def exists_word(word, instance):
-    return search_words_on_instance(word, instance)
+    return search_on_instance(word, instance)
 
 
 def search_by_word(word, instance):
-    return search_words_on_instance(word, instance, True)
+    return search_on_instance(word, instance, True)
