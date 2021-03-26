b568303173a45df2e2aef3bb114fd5828703432d
caiolmf <caio.lmf@gmail.com>
2021-03-26 13:18:08 -0300

    feat: file queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
+from collections import deque
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.file_queue = deque([])
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.file_queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.file_queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.file_queue.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+
+        return self.file_queue[index]
