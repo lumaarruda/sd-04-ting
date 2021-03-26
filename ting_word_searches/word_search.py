50d82606d1ccfc2bfb673a68751ffc35d4933ec4
lourenco <loulage@gmail.com>
2021-03-26 03:02:47 -0300

    bugfix -3

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,22 +1,22 @@
-def search_in_lines(word, file_info, add_content):
-    occurrences = []
+def search_on_line(word, file_info, add_content):
+    result = []
     for index in range(len(file_info["linhas_do_arquivo"])):
         line = file_info["linhas_do_arquivo"][index]
         if word.lower() in line.lower():
-            occurr = {"linha": index + 1}
+            occurrences = {"linha": index + 1}
             if add_content:
-                occurr["conteudo"] = line
-            occurrences.append(occurr)
-    return occurrences
+                occurrences["conteudo"] = line
+            result.append(occurrences)
+    return result
 
 
 def search_on_instance(word, instance, add_content=False):
     files_found = []
     for file_info in instance:
-        occurrences = search_in_lines(word, file_info, add_content)
+        occurrences = search_on_line(word, file_info, add_content)
 
-    if occurrences:
-        files_found.append(
+        if occurrences:
+            files_found.append(
                 {
                     "palavra": word,
                     "arquivo": file_info["nome_do_arquivo"],
