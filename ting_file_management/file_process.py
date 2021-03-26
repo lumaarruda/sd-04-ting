1e3611026047f47921b01dd615150ffb83450c65
lourenco <loulage@gmail.com>
2021-03-26 01:33:02 -0300

    implemented remove function on file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -22,7 +22,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos", file=sys.stdout)
+    file_name = instance.search(0)["nome_do_arquivo"]
+    instance.dequeue()
+    print(f"Arquivo {file_name} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
