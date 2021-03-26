9ef9f6a174e8fcbab9797137248aa49eeb668022
lourenco <loulage@gmail.com>
2021-03-26 01:27:41 -0300

    implemented file_process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def format_file(path_file, lines):
+    return {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+    new_file = format_file(path_file, lines)
+    instance.enqueue(new_file)
+    print(new_file, file=sys.stdout)
 
 
 def remove(instance):
