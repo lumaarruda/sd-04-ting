b59ec46b03d8277d74baf45d8bf9c0f20fef2196
lourenco <loulage@gmail.com>
2021-03-26 02:38:35 -0300

    implemented search_on_instance pfv funcione

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -10,9 +10,26 @@ def search_in_lines(word, file_info, add_content):
     return occurrences
 
 
+def search_on_instance(word, instance, add_content=False):
+    found = []
+    for file_info in instance:
+        occurrences = search_in_lines(word, file_info, add_content)
+
+    if occurrences:
+        found.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_info["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return found
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_on_instance(word, instance)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return search_on_instance(word, instance, True)
