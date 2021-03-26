6d53fc1248de685180cbc6ccbcacd364341fd757
lourenco <loulage@gmail.com>
2021-03-26 03:10:35 -0300

    vai pra amanha - search_on_lines funcionando - resto rever com calma

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -8,28 +8,3 @@ def search_on_lines(word, file_info, add_content):
                 occurrences["conteudo"] = line
             result.append(occurrences)
     return result
-
-
-def search_on_instance(word, instance, add_content=False):
-    files_found = []
-    for file_info in instance:
-        occurrences = search_on_lines(word, file_info, add_content)
-
-        if occurrences:
-            files_found.append(
-                {
-                    "palavra": word,
-                    "arquivo": file_info["nome_do_arquivo"],
-                    "ocorrencias": occurrences,
-                }
-            )
-
-    return files_found
-
-
-def exists_word(word, instance):
-    return search_on_instance(word, instance)
-
-
-def search_by_word(word, instance):
-    return search_on_instance(word, instance, True)
