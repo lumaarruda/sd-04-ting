ca3766d91ff649189631625601320e4be7b81206
Luma Arruda <luma.oarruda@gmail.com>
2021-03-26 16:31:11 -0300

    text importer com falha

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,15 +2,17 @@ import sys
 
 
 def txt_importer(path_file):
-    if path_file.endswith('txt'):
-        try:
-            with open(path_file, 'r') as file:
-                txt = []
-                file_lines = file.readlines()
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
 
-                for line in file_lines:
-                    txt.append(line.strip())
-                return txt
-        except FileNotFoundError:
-            return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
-    sys.stderr.write("Formato inválido\n")
+        with open(path_file, "r") as file:
+            text = []
+            lines = file.readlines()
+
+            for line in lines:
+                text.append(line.strip())
+            return text
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
