28395557c217be1320afbd326dddc26a99f6e49b
lourenco <loulage@gmail.com>
2021-03-26 02:35:06 -0300

    implemented search_in_lines helper function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,3 +1,15 @@
+def search_in_lines(word, file_info, add_content):
+    occurrences = []
+    for index in range(len(file_info["linhas_do_arquivo"])):
+        line = file_info["linhas_do_arquivo"][index]
+        if word.lower() in line.lower():
+            occur = {"linha": index + 1}
+            if add_content:
+                occur["conteudo"] = line
+            occurrences.append(occur)
+    return occurrences
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
 
