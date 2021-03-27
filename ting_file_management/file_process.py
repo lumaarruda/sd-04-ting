fc69f95b22cc36205bd8aa497460919c00aeb43d
mirellaalves <mirella.alves@gmail.com>
2021-03-26 22:28:36 -0300

    requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    file_read = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,  # Nome do arquivo recém adicionado
+        "qtd_linhas": len(file_read),  # Qtde de linhas existentes no arquivo
+        "linhas_do_arquivo": file_read  # linhas retornadas pela função do req2
+    }
+
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
 
 
 def remove(instance):
