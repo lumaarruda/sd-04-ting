973de635ed84a75357a4c98629dab933883687b2
Ilan Herbach <ilan.herbach@gmail.com>
2021-03-27 11:02:52 -0300

    3rd and 4th passing locally

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,28 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for element in range(len(instance)):
+        if instance.search(element)["nome_do_arquivo"] == path_file:
+            return
+    linhas_do_arquivo = txt_importer(path_file)
+    new_entry = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas_do_arquivo),
+        "linhas_do_arquivo": linhas_do_arquivo,
+    }
+    instance.enqueue(new_entry)
+    instance.last_element()
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() > 0:
+        file = instance.dequeue()
+        print(
+            f"Arquivo {file['nome_do_arquivo']} removido com sucesso"
+        )
+    else:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
