de5652baa772f681c014d172ada454ae3bda08dd
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-03-27 00:31:00 -0300

    process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in range(len(instance)):
+        if instance.search(item)['nome_do_arquivo'] == path_file:
+            return
 
+    data = txt_importer(path_file)
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+    result = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data,
+    }
 
-
-def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    instance.enqueue(result)
+    return print(result, file=sys.stdout)
