9e35d45ad5fc3b60dffd07bd2e9fb0d20013b111
pedrotpo <pedrotpoliveira@gmail.com>
2021-03-26 21:11:50 -0300

    Req 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -27,4 +27,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
+    else:
+        sys.stdout.write(f"{data}")
