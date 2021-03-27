a187d9aa9c23b85f21d7e9f2660d08f2977e43bc
mirellaalves <mirella.alves@gmail.com>
2021-03-27 00:58:45 -0300

    work on requirements 6 and 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,30 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    line_list = []
+    result = []
+    for index in range(len(instance)):
+        file_checked = instance.search(index)
+        if word in file_checked:
+            line_list.append({"linha": index + 1})
+            result.append({
+                "palavra": word,
+                "arquivo": file_checked["nome_do_arquivo"],
+                "ocorrencias": line_list
+            }.rstrip("\n"))
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    line_list = []
+    result = []
+    for index in range(len(instance)):
+        file_checked = instance.search(index)
+        if word in file_checked:
+            line_list.append({
+                "linha": index + 1,
+                "conteudo": file_checked["linhas_do_arquivo"]})
+            result.append({
+                "palavra": word,
+                "arquivo": file_checked["nome_do_arquivo"],
+                "ocorrencias": line_list
+            }.rstrip("\n"))
+    return result
