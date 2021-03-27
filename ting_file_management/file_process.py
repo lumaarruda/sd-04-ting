d0bdd4e563c85ac567fd0f1f5fbcdf3864602758
richardwellerson <richardwellerson@gmail.com>
2021-03-27 10:47:04 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,12 @@
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+
+    lines_file = txt_importer(path_file)
+    new_file = format(path_file, lines_file)
+    instance.enqueue(new_file)
+    print(new_file, file=sys.stdout)
 
 
 def remove(instance):
