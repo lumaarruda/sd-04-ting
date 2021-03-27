83628899f78571c2110df1cd024e9f91c6d511b9
mirellaalves <mirella.alves@gmail.com>
2021-03-27 13:11:35 -0300

    correct search_by_word function, requirement 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,14 +20,16 @@ def search_by_word(word, instance):
     for index in range(len(instance)):
         file_checked = instance.search(index)
         file_line = file_checked["linhas_do_arquivo"]
-        if word in file_line[0]:
-            line_list.append({
-                "linha": index + 1,
-                "conteudo": f"{file_line}"
-            })
-            result.append({
-                "palavra": f"{word}",
-                "arquivo": file_checked["nome_do_arquivo"],
-                "ocorrencias": line_list
-            })
+        for i, line in enumerate(file_line):
+            if word.lower() in line.lower():
+                line_list.append({
+                    "linha": i + 1,
+                    "conteudo": f"{line}"
+                })
+                result.append({
+                    "palavra": f"{word}",
+                    "arquivo": file_checked["nome_do_arquivo"],
+                    "ocorrencias": line_list
+                })
     return result
+# bug fixed with leandro marten support
